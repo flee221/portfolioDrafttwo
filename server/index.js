31d4f850f5d55f77f2afa004d60f8a47e23e77f4
fliss <felicity.sarah.lee@gmail.com>
2026-05-21 11:12:15 +0100

    basic structure

--- server/index.js
+++ server/index.js
@@ -29,114 +29,30 @@ app.get("/ascii", async (req, res) => {
   }
 });
 
-app.get("/github", async (req, res) => {
-  //Uses express to define an endpoint on the server
-  try {
-    const { username } = req.query;
-    //places parameter into variable (received from front end)
-    const response = await fetch(`https://api.github.com/users/${username}`, {
-      headers: {
-        Authorization: `Bearer ${githubKey}`,
-        //send across API key alongside request
-        Accept: "application/vnd.github+json",
-        //result format must be json
-      },
-      //backend acts as client for http request to github API
-    });
-    const data = await response.json();
-    //converts into readable forman
+// app.get("/github", async (req, res) => {
+//   //Uses express to define an endpoint on the server
+//   try {
+//     const { username } = req.query;
+//     //places parameter into variable (received from front end)
+//     const response = await fetch(`https://api.github.com/users/${username}`, {
+//       headers: {
+//         Authorization: `Bearer ${githubKey}`,
+//         //send across API key alongside request
+//         Accept: "application/vnd.github+json",
+//         //result format must be json
+//       },
+//       //backend acts as client for http request to github API
+//     });
+//     const data = await response.json();
+//     //converts into readable forman
 
-    res.json({ data });
-    //returns to frontend
-  } catch (err) {
-    console.log(err);
-    //try catch block to catch errors and log in console
-  }
-});
-
-app.get("/github/repos", async (req, res) => {
-  try {
-    const { username } = req.query;
-
-    const response = await fetch(
-      `https://api.github.com/users/${username}/repos`,
-      {
-        headers: {
-          Authorization: `Bearer ${githubKey}`,
-          Accept: "application/vnd.github+json",
-        },
-      },
-    );
-    const data = await response.json();
-
-    res.json({ data });
-  } catch (err) {
-    console.log(err);
-  }
-});
-
-app.get("/github/languages", async (req, res) => {
-  try {
-    const { username } = req.query;
-    const fetchRepos = await fetch(
-      `https://api.github.com/users/${username}/repos`,
-      {
-        headers: {
-          Authorization: `Bearer ${githubKey}`,
-          Accept: "application/vnd.github+json",
-        },
-      },
-    );
-
-    const allRepos = await fetchRepos.json();
-    //place into variable
-    const languageStats = [];
-    const total = {};
-    for (const repo of allRepos) {
-      const res = await fetch(repo.languages_url, {
-        headers: {
-          Authorization: `Bearer ${githubKey}`,
-          Accept: "application/vnd.github+json",
-        },
-      });
-      //second call to get language statistics for all repos
-
-      const data = await res.json();
-      languageStats.push(data);
-      //add language statistics in array
-    }
-
-    languageStats.forEach((languages) => {
-      for (const langauge in languages) {
-        total[langauge] = (total[langauge] || 0) + languages[langauge];
-        //cumulative sum of all languages
-      }
-    });
-
-    res.json({ data: total });
-  } catch (err) {
-    console.log(err);
-  }
-});
-
-app.get("/github/repo", async (req, res) => {
-  try {
-    const { username, reponame } = req.query;
-    const response = await fetch(
-      `https://api.github.com/repos/${username}/${reponame}`,
-      {
-        headers: {
-          Authorization: `Bearer ${githubKey}`,
-          Accept: "application/vnd.github+json",
-        },
-      },
-    );
-    const data = await response.json();
-    res.json({ data });
-  } catch (err) {
-    console.log(err);
-  }
-});
+//     res.json({ data });
+//     //returns to frontend
+//   } catch (err) {
+//     console.log(err);
+//     //try catch block to catch errors and log in console
+//   }
+// });
 
 app.use(express.static(path.join(process.cwd(), "../client")));
 app.use(express.static(path.join(process.cwd(), "../client/public")));
